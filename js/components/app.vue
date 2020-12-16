9107a14260784e2432a2b07a099dde5b23c13696
GabrielCamarero <bessa.3301@gmail.com>
2020-12-16 17:34:44 -0300

    debugging router

---
+++ js/components/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flexible-content">
+    <main>
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script>
+import Home from './home/home.vue'
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
--- js/index.js
+++ js/index.js
@@ -1,14 +1,16 @@
 import '../css/main.css'
 import Vue from 'vue'
-import Game from './components/Game'
+//import Game from './components/Game'
+import App from './components/app.vue'
 import store from './vuex/store'
+import router from './router.js'
 
 // the main entrance
 /* eslint-disable no-new */
 new Vue({
-  el: '#application',
+  router,
+  store,
   render(h) {
-    return h(Game)
-  },
-  store
-})
+    return h(App)
+  }
+}).$mount('#app')
