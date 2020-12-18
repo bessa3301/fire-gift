3e58b70ba6d9beb56200954b18f18e3d6b8f5da3
kvass33 <bessa.3301@gmail.com>
2020-12-18 00:56:00 -0300

    fixing issues on docker / webpack

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,9 @@ WORKDIR /app
 
 COPY package.json package.json
 COPY yarn.lock yarn.lock
+COPY .babelrc .babelrc
+COPY webpack.commom.js webpack.commom.js
+COPY webpack.config.prod.js webpack.config.prod.js
 
 RUN npm install yarn 
 RUN npm install -g serve
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,6 @@
 const { resolve } = require('path')
 const { merge } = require('webpack-merge')
-const common = require('./webpack.common')
+const common = require('./webpack.common.js')
 const webpack = require('webpack')
 
 module.exports = merge(common, {
